8f6e5ab1ce6dc4ba7c718e78e377f8b491988914
Metrarona <137288016+Metrarona@users.noreply.github.com>
2024-09-25 06:24:00 -0300

    new

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import CalculadoraIdade from './calculadoraIdade'
+
+function App() {
+ return (
+  <div>
+    <h1>Calculadora de Idade</h1>
+    <CalculadoraIdade/>
+  </div>
+ )
+}
+
+export default App
